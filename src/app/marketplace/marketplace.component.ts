efd4298fcaa7ad6387adb97b2e7427285c8d8cb6
brandon <bcooper085@gmail.com>
2017-04-03 13:09:42 -0700

    add about and marketplace pages with routing

---
+++ src/app/marketplace/marketplace.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-marketplace',
+  templateUrl: './marketplace.component.html',
+  styleUrls: ['./marketplace.component.css']
+})
+export class MarketplaceComponent {
+
+}
